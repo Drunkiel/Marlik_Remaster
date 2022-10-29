bfe0fb9f208b5bbed679df182335a18ca6111110
Drunkiel <drunkielfel@gmail.com>
2022-10-29 20:26:17 +0200

    Added shop controller:

---
+++ Assets/Scripts/InteractiveObjects/ShopController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ShopController : MonoBehaviour
+{
+    public OpenCloseUI _openCloseUI;
+    TriggerController _triggerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _triggerController = GetComponent<TriggerController>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_triggerController.isTriggered && Input.GetKeyDown(KeyCode.E)) _openCloseUI.OpenClose();
+    }
+}
