dc926dcc519a135da356939e8d54f89cd9fc1749
Drunkiel <drunkielfel@gmail.com>
2022-11-19 17:28:07 +0100

    Small update

--- Assets/Scripts/MapGenerator/IndywidualMaps/WallRun/WallRunController.cs
+++ Assets/Scripts/MapGenerator/IndywidualMaps/WallRun/WallRunController.cs
@@ -4,10 +4,17 @@ public class WallRunController : MonoBehaviour
 {
     public Transform startPoint;
     public WallController _wallController;
+    LoadingScreen _loadingScreen;
+
+    void Start()
+    {
+        _loadingScreen = GameObject.FindGameObjectWithTag("LoadingScreen").GetComponent<LoadingScreen>();
+    }
 
     public void ResetMap()
     {
-        _wallController.transform.localPosition = new Vector2(-30f, 6f);
+        _loadingScreen.StartCoroutine("StartLoading", "Szybciej tymi nó¿kami ruszaj...");
+        _wallController.transform.localPosition = new Vector2(-34f, 3.87f);
         _wallController.player.position = startPoint.position;
     }
 }
